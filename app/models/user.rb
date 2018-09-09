a4fb5c70375c3b8ea1bdd5480324914077edeb11
dcheley <danielcheley@hotmail.com>
2018-09-09 14:41:30 -0400

    Adding name column, field, param, etc to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 
   has_many :attendances
 
+  validates :name, presence: true
   validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }, uniqueness: true
   validates :password, length: { in: 8..16 }, format: { with: /\A((?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[!@#$%&])).*\Z/, message: "must include at least one lowercase letter, one uppercase letter, one digit (1234567890) and symbol (!@#$%&)" }, on: [:create, :update]
 end
